aca6d6bfe9e83c0af3c153b12b813ee815a92fc2
Khaleel-4005 <khaleel4005@gmail.com>
2025-01-03 18:29:49 +0530

    UPDATE.ASYNC-AWAIT,IIFE

--- SYNCHRONOUS/Async.Script.js
+++ SYNCHRONOUS/Async.Script.js
@@ -232,3 +232,80 @@ asynFunc1().then((res) => {
 });
 
 
+
+// ASYNC-AWAIT:
+
+
+function api() {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+        console.log("wheather data");
+        resolve(200);
+        },2000);
+    });
+}
+
+
+async function getWeatherData(){
+    await api(); //1st
+    await api(); //2nd
+}
+
+function getData(dataId){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("date", dataId);
+                resolve("success");
+    
+            }, 2000);
+        });
+    }
+    
+
+
+
+// ASYNC-AWAIT:
+
+async function getAllData() {
+    console.log("getting data1....");
+    await getData(1);
+    console.log("getting data2....");
+    await getData(2);
+    console.log("getting data3....");
+    await getData(3);
+    console.log("getting data4....");
+    await getData(4);
+    console.log("getting data5....");
+    await getData(5);
+    console.log("getting data6....");
+    await getData(6);
+    console.log("getting data7....");
+    await getData(7);
+}
+
+
+// IIFE:
+
+
+async function getAllData() {
+    console.log("getting data1....");
+    await getData(1);
+    console.log("getting data2....");
+    await getData(2);
+    console.log("getting data3....");
+    await getData(3);
+
+}
+
+(async function () {
+    console.log("getting data1....");
+    await getData(1);
+    console.log("getting data2....");
+    await getData(2);
+    console.log("getting data3....");
+    await getData(3);
+
+})();
+
+
+
