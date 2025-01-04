23a995b876b9e27d45095e19cebfac63326e360d
Khaleel-4005 <khaleel4005@gmail.com>
2025-01-04 22:48:53 +0530

    UPDATED.ASYNC-AWAIT

--- SYNCHRONOUS/Async.Script.js
+++ SYNCHRONOUS/Async.Script.js
@@ -309,3 +309,20 @@ async function getAllData() {
 
 
 
+
+async function getAllData() {
+    console.log("getting data1....");
+    await getData(1);
+    console.log("getting data2....");
+    await getData(2);
+    console.log("getting data3....");
+    await getData(3);
+    console.log("getting data4....");
+    await getData(4);
+    console.log("getting data5....");
+    await getData(5);
+    console.log("getting data6....");
+    await getData(6);
+    console.log("getting data7....");
+    await getData(7);
+}
